911eae517fffb77ebf7282c69f1dfcd87a852778
Wyatt Barnes <wyatt@filestack.com>
2017-06-05 10:43:13 -0500

    Basic Hello World with varible being interpreted

---
+++ c#/hello_world.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApplication
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      var name = "Wyatt";
+      Console.WriteLine($"Hello {name} :)");
+    }
+  }
+}
